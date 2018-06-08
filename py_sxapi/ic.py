8dbebea923944fc08aab21a232ece7ebf9bc1655
David Briggs <47540344+pss-dbriggs@users.noreply.github.com>
2018-06-08 17:34:33 -0500

    REST interface is now working. Plumbing for making more general

--- py_sxapi/ic.py
+++ py_sxapi/ic.py
@@ -13,12 +13,13 @@ import datetime
 class ICService:
     _mode = 'test'
     _debug = False
-    _logfile = ''
+    _logfile = '/home/dbriggs/environments/sxe_item_import/test_log.log'
+    _endpoint = ''
+    _credentials = {}
     
     def __init__(self, mode = 'test', debug=False):
         self._mode = mode
-        self._debug = debug
-        self._endpoint     
+        self._debug = debug  
         
         if self._mode == 'prod':
             self.endpoint = 'http://psssxe2:8185/rest/sxapirestservice/'
@@ -62,27 +63,42 @@ class ICService:
             'OperatorPassword':credentials['password']}
         return connection_info
 
-    def send_request(self, function, headers):
+    def send_request(self, function, data):
 
         if self._debug and self._logfile != '': #write to log if debug is turned on
             with open(self._logfile, 'a') as logf:
 
-                logf.write(request)
+                logf.write(json.dumps(data))
                 logf.write('\n')
 
-        response = requests.get(self.endpoint+function, headers=headers)         
+        #requests only accepts strings in the headers, but we want to pass json
+
+        response = requests.post(self.endpoint+function, json=data)         
         
         if response.status_code == requests.codes.ok:
-            with open(logfile, 'a') as logf:
-                print(response, file=logf)
+            with open(self._logfile, 'a') as logf:
+                print(response.text, file=logf)
         
         else:
-            with open(logfile, 'a') as logf:
-                print(response, file=logf)
+            with open(self._logfile, 'a') as logf:
+                print(response.text, file=logf)
         return response
 
-    def check_product(self, credentials, product):
-        #request={ 'ProductCode':product } legacy SOAP request
+    def check_product(self, product, credentials=None):
+        """
+        Checks for the presence of a product. Uses the sxapiicgetproductlistv2 Call.
+        Input:
+            -credentials, a dictionary containing three items, which are used in 
+            creating the connection:
+                -cono: the SXe Company Number in the callConnection object
+                -username: the initials of the SXe operator making the call
+                -password: the password of the SXe operating making the call 
+            -product, the product number to check for
+        Output: True if the product exists, false if not.
+        """
+        if credentials is None and self._credentials != {}:
+            credentials = self._credentials
+
         request={'request':                         #the request payload. See ICGetProductListV2 in the SXAPI docs for more information on structure
             {
                 'companyNumber': credentials['cono'],
@@ -96,17 +112,34 @@ class ICService:
         #    callConnection=connection_info, 
         #    request=request)
 
-        response = send_request(function='sxapiicgetproductlistv2', headers=request)
+        response = self.send_request(function='sxapiicgetproductlistv2', data=request)
 
         response_dict = response.json()
 
-        if response_dict['Outproduct'] is not None:
+        l = response_dict['response']['tProdv2']['t-prodv2']
+        items = [item['prod'] for item in l]
+
+        if product in items:
             return True
         return False
         #return response_dict
 
-    def check_product_warehouse(self, credentials, product, warehouse):
-        #request={ 'Product':product,'Whse':warehouse }
+    def check_product_warehouse(self, product, warehouse, credentials=None):
+        """
+        Checks for the presence of a product in a particular warehouse. Uses the sxapiicgetwhseproductdatageneralv2 Call.
+        Input:
+            -credentials, a dictionary containing three items, which are used in 
+            creating the connection:
+                -cono: the SXe Company Number in the callConnection object
+                -username: the initials of the SXe operator making the call
+                -password: the password of the SXe operating making the call 
+            -product, the product number to check for
+            -warehouse, the warehouse to look in
+        Output: True if the product is present, false if not. Fails on any error other than 'Product/Warehouse Not Set Up'
+        """
+        if credentials is None and self._credentials != {}:
+            credentials = self._credentials
+
         request={'request':                         #the request payload. See ICGetWhseProductDataGeneral in the SXAPI docs for more information on structure
             {
                 'companyNumber': credentials['cono'],
@@ -117,16 +150,19 @@ class ICService:
             }
         }
 
-        response = send_request(function='sxapiicgetwhseproductdatageneralv2', headers=request)
+        response = self.send_request(function='sxapiicgetwhseproductdatageneralv2', data=request)
 
         response_dict = response.json()
 
         #return response_dict
-        if response_dict['ErrorMessage'] is None:
+        if response_dict['response']['cErrorMessage'] == 'Product/Warehouse Not Set Up in Warehouse Products - ICSW (4602)':
+            return False
+        elif response_dict['response']['cErrorMessage'] != '':
+            raise ValueError('Cannot validate ICSW record')
+        else:
             return True
-        return False
 
-    def item_import(self, credentials, file):
+    def item_import(self, file, credentials=None):
         """
         Import items based on file input. Uses the sxapiICProductMnt Call.
         Input:
@@ -140,6 +176,8 @@ class ICService:
         Output: A JSON array consisting of two lists: ErrorMessage and ReturnData
         """
         #connection_info = self.create_credentials(credentials)
+        if credentials is None and self._credentials != {}:
+            credentials = self._credentials
 
         chg_list = []
 
@@ -156,12 +194,12 @@ class ICService:
                 update_mode = 'chg'
                 
                 if key2 != '':
-                    if self.check_product_warehouse(credentials, key1, key2) == True:
+                    if self.check_product_warehouse(key1, key2, credentials) == True:
                         update_mode = 'chg'
                     else:
                         update_mode = 'add'
                 else:
-                    if self.check_product(credentials, key1) == True:
+                    if self.check_product(key1, credentials) == True:
                         update_mode = 'chg'
                     else:
                         update_mode = 'add'
@@ -175,7 +213,7 @@ class ICService:
                         tmp_dict['key2'] = key2
                         tmp_dict['seqNo'] = seq_no
                         tmp_dict['setNo'] = set_no
-                        tmp_dict['updateMode'] = 'add'
+                        tmp_dict['updateMode'] = update_mode
                         chg_list.append(tmp_dict)
                         seq_no += 1
                 
@@ -188,28 +226,29 @@ class ICService:
                 'companyNumber': credentials['cono'],
                 'operatorInit' : credentials['username'],
                 'operatorPassword' : credentials['password'],
-                'tMntTt' : chg_list
+                'tMntTt' : {'t-mnt-tt':chg_list}
             }
         }
         
-        response = send_request(function='sxapiicproductmnt', headers=request)
+        response = self.send_request(function='sxapiicproductmnt', data=request)
 
         response_dict = response.json()
+        return_dict = {}
 
-        if response_dict['ErrorMessage'] is not None:
-            errors = response_dict['ErrorMessage'].split('|')
-            response_dict['ErrorMessage'] = errors
+        if response_dict['response']['cErrorMessage'] is not None:
+            errors = response_dict['response']['cErrorMessage'].split('|')
+            return_dict['ErrorMessage'] = errors
 
-        if response_dict['ReturnData'] is not None:
-            return_data = response_dict['ReturnData'].split('|')
-            response_dict['ReturnData'] = return_data
+        if response_dict['response']['returnData'] is not None:
+            return_data = response_dict['response']['returnData'].split('|')
+            return_dict['ReturnData'] = return_data
 
         #print dir(response)
         if self._logfile != '':
             with open(self._logfile, 'a') as logf:
-                print("%s: ICProductMnt - %s" % (datetime.datetime.utcnow(), json.dumps(response_dict)), file=logf)
+                print("%s: ICProductMnt - %s" % (datetime.datetime.utcnow(), json.dumps(return_dict)), file=logf)
 
-        return json.dumps(response_dict)
+        return json.dumps(return_dict)
 
 
     
